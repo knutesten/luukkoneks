3e690ba0b611784c6e95e920fa3661ae3d259d1d
Aleksi Luukkonen <alu@capraconsulting.no>
2020-03-03 17:57:38 +0100

    Fix memory impl

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod register;
+mod memory;
 
 fn main() {
     let registers = register::Registers::new();
 
     println!("{:#?}", registers);
+
+    let memory = memory::Memory::init_from_rom("Tetris.gb").expect("Shit ass");
+
+    println!("Loaded shit!")
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,33 +1,41 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::io;
 
+#[derive(Clone,Eq)]
 pub struct Memory {
     cartridge_mem: Vec<u8>,
     working_mem: Vec<u8>
 }
 
+impl PartialEq for Memory {
+    fn eq(&self, other: &Self) -> bool {
+        self.cartridge_mem.eq(&other.cartridge_mem) &&
+        self.working_mem.eq(&other.working_mem)
+    }
+}
 impl Memory {
-    pub fn init_from_rom(rom_name: string) -> Memory {
-        let mut file = File::open("Tetris.gb")?;
-        let mem = Memory {
+    pub fn init_from_rom(rom_name: &str) -> Result<Memory, io::Error> {
+        let mut file = File::open(rom_name)?;
+        let mut mem = Memory {
             cartridge_mem: Vec::new(),
             working_mem: Vec::with_capacity(8*1024)
         };
         file.read_to_end(&mut mem.cartridge_mem)?;
-        mem
+        Ok(mem)
     }
 
-    fn read_memory(self, addr: u16) -> u8 {
+    fn read(self, addr: u16) -> u8 {
         return match addr {
-            0..0x7FFF => self.cartridge_mem[addr],
-            0xC000..0xDFFF => self.working_mem[addr-0xC000],
+            0..=0x7FFF => self.cartridge_mem[addr as usize],
+            0xC000..=0xDFFF => self.working_mem[(addr-0xC000) as usize],
             default => panic!("Tried to read invalid memory {:#x}", addr)
         }
     }
 
-    fn write_memory(self, addr: u16, data: u8) {
-        match(addr) {
-            0xC000..0xDFFF => self.working_mem[addr-0xC000] = data,
+    fn write(&mut self, addr: u16, data: u8) {
+        match addr {
+            0xC000..=0xDFFF => self.working_mem[(addr-0xC000) as usize] = data,
             default=> panic!("Invalid addr to write memory {:#x}", addr)
         }
     }
