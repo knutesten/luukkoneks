531b0f498132eab1fa3230cc6351874dadfa30db
Knut Esten Melandsø Nekså <knutn@netcompany.com>
2020-03-03 18:44:33 +0100

    Depends on opcode

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod register;
 mod memory;
+mod opcode;
 
 fn main() {
     let registers = register::Registers::new();
-
     println!("{:#?}", registers);
-
     let memory = memory::Memory::init_from_rom("Tetris.gb").expect("Shit ass");
-
     println!("Loaded shit!")
 }
